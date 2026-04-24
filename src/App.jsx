3c081296923502cdeb7af52673dd77a4ca952be0
HexKernel <187540092+HexKernel@users.noreply.github.com>
2026-04-24 15:25:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ const QUESTIONS = [
 ];
 
 const MEMES = [
-  `${import.meta.env.BASE_URL}/photo_1.jpg`,
-  `${import.meta.env.BASE_URL}/photo_2.jpg`,
-  `${import.meta.env.BASE_URL}/photo_3.jpg`
+  `${import.meta.env.BASE_URL}photo_1.jpg`,
+  `${import.meta.env.BASE_URL}photo_2.jpg`,
+  `${import.meta.env.BASE_URL}photo_3.jpg`
 ];
 
 function shuffle(arr) {
